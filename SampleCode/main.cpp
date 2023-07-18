69086b1ee86b6014bf7ad6399de3e582553f7883
HWNOTE <ekdrn1@naver.com>
2023-07-18 16:06:22 +0900

    문자열에서 공백을 제거하고(erase-remove_if idiom), regex를 통해서 숫자를 추출하는 코드

--- SampleCode/main.cpp
+++ SampleCode/main.cpp
@@ -1,38 +1,41 @@
+#include <regex>
 #include <string>
-#include <Windows.h>
 
-// Convert a wide Unicode string to an UTF8 string
-std::string utf8_encode(const std::wstring& wstr)
+int main()
 {
-	int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
-	std::string strTo(size_needed, 0);
-	WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
-	return strTo;
-}
+	std::string FindThread = "  [  numthreads (   128  ,  1  ,  1) ]";
 
-// Convert an UTF8 string to a wide Unicode String
-std::wstring utf8_decode(const std::string& str)
-{
-	int size_needed = MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), NULL, 0);
-	std::wstring wstrTo(size_needed, 0);
-	MultiByteToWideChar(CP_UTF8, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
-	return wstrTo;
-}
+	//std::regex NumThreadRegex(R"(\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\))");
+	std::regex NumThreadRegex(R"(.*\((\d+),(\d+),(\d+\)).*)");
 
-// Convert an wide Unicode string to ANSI string
-std::string unicode2ansi(const std::wstring& wstr)
-{
-	int size_needed = WideCharToMultiByte(CP_ACP, 0, &wstr[0], -1, NULL, 0, NULL, NULL);
-	std::string strTo(size_needed, 0);
-	WideCharToMultiByte(CP_ACP, 0, &wstr[0], (int)wstr.size(), &strTo[0], size_needed, NULL, NULL);
-	return strTo;
-}
+	size_t pos = FindThread.find("numthreads");
+	if (std::string::npos != pos)
+	{
+		FindThread.erase(std::remove_if(FindThread.begin(), FindThread.end(),
+			[](char c)->bool
+			{
+				return std::isspace(c);
+			}), FindThread.end());
 
-// Convert an ANSI string to a wide Unicode String
-std::wstring ansi2unicode(const std::string& str)
-{
-	int size_needed = MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), NULL, 0);
-	std::wstring wstrTo(size_needed, 0);
-	MultiByteToWideChar(CP_ACP, 0, &str[0], (int)str.size(), &wstrTo[0], size_needed);
-	return wstrTo;
+		int NumThreads[3]{};
+		std::smatch Matches;
+		if (std::regex_match(FindThread, Matches, NumThreadRegex))
+		{
+			if (Matches.size() < 4)
+			{
+				return 0;
+			}
+			
+			for (size_t i = 1; i < Matches.size(); ++i)
+			{
+				NumThreads[i - 1] = std::stoi(Matches[i]);
+			}
+		}
+
+
+		int breakpoint = 0;
+	}
+
+
+	return 0;
 }
