2acc315cc60ca30e19ea57bf12e5100ed25a4770
HWNOTE <ekdrn1@naver.com>
2023-07-19 17:28:24 +0900

    std::span 공부

--- SampleCode/main.cpp
+++ SampleCode/main.cpp
@@ -1,41 +1,47 @@
-#include <regex>
 #include <string>
+#include <vector>
+#include <span>
+#include <iostream>
 
 int main()
 {
-	std::string FindThread = "  [  numthreads (   128  ,  1  ,  1) ]";
+	std::vector<int> vec = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	//std::regex NumThreadRegex(R"(\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\))");
-	std::regex NumThreadRegex(R"(.*\((\d+),(\d+),(\d+\)).*)");
+	std::span<int> sp(vec.begin() + 5, vec.end());
 
-	size_t pos = FindThread.find("numthreads");
-	if (std::string::npos != pos)
+	
+	int a = 0;
+	//반복자를 레퍼런스 형태로 받을 시 값이 수정된다
+	for (auto& iter : sp)
 	{
-		FindThread.erase(std::remove_if(FindThread.begin(), FindThread.end(),
-			[](char c)->bool
-			{
-				return std::isspace(c);
-			}), FindThread.end());
+		iter = a;
+		++a;
+	}
+	//이런식으로 직접 접근 수정도 가능함
+	sp[0] = 3;
 
-		int NumThreads[3]{};
-		std::smatch Matches;
-		if (std::regex_match(FindThread, Matches, NumThreadRegex))
-		{
-			if (Matches.size() < 4)
-			{
-				return 0;
-			}
-			
-			for (size_t i = 1; i < Matches.size(); ++i)
-			{
-				NumThreads[i - 1] = std::stoi(Matches[i]);
-			}
-		}
+	//span은 시작점 포인터와 데이터 갯수만 들고 있으므로 데이터가 크지 않음
+	std::cout << "Size of span = " << sizeof(sp) << std::endl;
 
 
-		int breakpoint = 0;
+
+	//const를 붙여버리면 수정이 불가능
+	std::span<int const> sp2(vec.begin() + 5, vec.end());
+	
+	//컴파일 에러
+	//sp2[3] = 3;
+	for (size_t i = 0; i < vec.size(); ++i)
+	{
+		std::cout << vec[i] << std::endl;
 	}
 
+
+	//빈 span을 순회돌면 아무 일도 일어나지 않는다
+	std::span<int> EmptySp;
+	for (auto iter : EmptySp)
+	{
+		std::cout << iter << std::endl;
+	}
 
 	return 0;
 }
