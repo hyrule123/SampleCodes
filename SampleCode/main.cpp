48a4414cc1c3660a53fe675db11e8d96a88ae1c4
HWNOTE <ekdrn1@naver.com>
2023-08-01 20:38:10 +0900

    별도로 정의한 데이터 구조를 문자열에 담아서 저장하기 결론: 저장 가능

--- SampleCode/main.cpp
+++ SampleCode/main.cpp
@@ -2,8 +2,59 @@
 #include <typeinfo>
 #include <typeindex>
 
-int main() {
-    std::cout << "Size of std::type_info: " << sizeof(std::type_info) << " bytes" << std::endl;
-    std::cout << "Size of std::type_index: " << sizeof(std::type_index) << " bytes" << std::endl;
+#include "json-cpp/json.h"
+
+struct Vector3
+{
+    float V[3];
+    Vector3() : V{} {}
+    Vector3(float _x, float _y, float _z) : V{ _x, _y, _z } {}
+};
+
+struct Matrix
+{
+    float M[16];
+};
+
+int main() 
+{
+    {
+        Vector3 TestVec = Vector3(1.f, 2.f, 3.f);
+
+        std::string Vec2Str((const char*)&TestVec, sizeof(TestVec));
+
+        Json::Value jVal;
+        jVal["Test"] = Vec2Str;
+
+        std::string Str2Vec = jVal["Test"].asString();
+        Vector3 ReturnVec{};
+        memcpy(&ReturnVec, Str2Vec.data(), sizeof(Vector3));
+    }
+
+    {
+        Matrix TestMat{};
+        for (int i = 0; i < 16; ++i)
+        {
+            TestMat.M[i] = (float)i;
+        }
+
+        std::string Mat2Str((const char*)&TestMat, sizeof(TestMat));
+
+        Json::Value jVal;
+        jVal["Test"] = Mat2Str;
+
+        std::string Str2Mat = jVal["Test"].as<std::string>();
+        Matrix ReturnVec{};
+        memcpy(&ReturnVec, Str2Mat.data(), sizeof(Matrix));
+
+
+        size_t MatSize = sizeof(Matrix);
+        size_t MatToStrSize = Str2Mat.size();
+
+        int a = 0;
+    }
+
+
+
     return 0;
 }
