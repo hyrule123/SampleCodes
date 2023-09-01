8c0267010b9a48bd932bf694eb85912ee3a07546
HWNOTE <ekdrn1@naver.com>
2023-09-01 16:51:03 +0900

    Current Path를 바꾸면 Relative Path도 바뀐다

--- SampleCode/main.cpp
+++ SampleCode/main.cpp
@@ -1,111 +1,27 @@
-﻿#include <iostream>
+﻿#include <filesystem>
+#include <iostream>
 
-//전방 선언
-template <typename T>
-unsigned int getTypeID(const T* typeTag = nullptr);
-
-// Custom type tag class
-class TypeTag 
+namespace std
 {
-public:
-    TypeTag() {};
-    virtual ~TypeTag() {};
-
-
-    virtual unsigned int GetType() { return getTypeID<TypeTag>(); }
-#define TYPE_TAG(_ty) virtual unsigned int GetType() { return getTypeID<_ty>(); }
-
-private:
-    // Generate a unique type ID for each tag
-    static unsigned int getNextTypeId() 
-    {
-        static unsigned int typeId = 0;
-        return ++typeId;
-    }
-
-    template <typename T>
-    friend unsigned int getTypeID(const T* typeTag);
-};
-
-// Function to get type ID based on type tag
-template <typename T>
-unsigned int getTypeID(const T* typeTag)
-{
-    static unsigned int typeId = TypeTag::getNextTypeId();
-    return typeId;
+	namespace fs = filesystem;
 }
 
-class DerivedA : public TypeTag
+int main()
 {
-public:
-    DerivedA() {};
-    virtual ~DerivedA() {};
+	std::fs::path add;
+	add /= "x64";
+	std::fs::current_path(add);
+	
+	std::string newpath = std::fs::current_path().string();
+	std::cout << newpath << std::endl;
 
-    TYPE_TAG(DerivedA);
-};
+	std::fs::path relPath = ".";
+	std::fs::path absPath = std::fs::absolute(relPath);
+	std::cout << absPath.string() << std::endl;
 
-class DerivedB : public TypeTag
-{
-public:
-    DerivedB() {};
-    virtual ~DerivedB() {};
-
-    TYPE_TAG(DerivedB);
-};
-
-class DerivedC : public DerivedB
-{
-public:
-    DerivedC() {};
-    virtual ~DerivedC() {};
-
-    TYPE_TAG(DerivedC);
-};
-
-
-// Type-specific tags
-struct IntTypeTag : public TypeTag {};
-struct DoubleTypeTag : public TypeTag {};
-struct StringTypeTag : public TypeTag {};
-struct TripleTypeTag : public DoubleTypeTag {};
+	bool b = std::filesystem::exists("asdf/asdfasdf/");
 
 
 
-
-int main() {
-    // Get type IDs
-    IntTypeTag itg{};
-    unsigned int intTypeId0 = getTypeID(&itg);
-    unsigned int intTypeId1 = getTypeID<IntTypeTag>();
-
-    DoubleTypeTag dtg0{};
-    unsigned int doubleTypeId0 = getTypeID(&dtg0);
-
-    DoubleTypeTag dtg1{};
-    unsigned int doubleTypeId1 = getTypeID(&dtg1);
-
-    unsigned int doubleTypeId2 = getTypeID<DoubleTypeTag>();
-
-    StringTypeTag stg0{};
-    unsigned int stringTypeId0 = getTypeID(&stg0);
-    unsigned int stringTypeId1 = getTypeID<StringTypeTag>();
-    unsigned int stringTypeId2 = getTypeID(&stg0);
-
-
-    unsigned int TripleTypeId0 = getTypeID<TripleTypeTag>();
-    unsigned int TripleTypeId1 = getTypeID<TripleTypeTag>();
-
-
-    TripleTypeTag tg{};
-    unsigned int ttI = getTypeID(&tg);
-
-    DerivedA a{};
-    std::cout << a.GetType() << std::endl;
-    DerivedB b{};
-    std::cout << b.GetType() << std::endl;
-    DerivedC c{};
-    std::cout << c.GetType() << std::endl;
-
-
-    return 0;
+	return 0;
 }
