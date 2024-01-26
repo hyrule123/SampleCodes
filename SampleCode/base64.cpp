46e68a7a2fff0362ee9455c20d0675f06cf1599c
HWNOTE <ekdrn1@naver.com>
2024-01-26 16:35:59 +0900

    base64 src, dest 포인터 주소 받는 함수 추가

--- SampleCode/base64.cpp
+++ SampleCode/base64.cpp
@@ -28,7 +28,7 @@
 #include "base64.h"
 #include <iostream>
 
-static const std::string base64_chars = 
+constexpr std::string_view base64_chars = 
              "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
              "abcdefghijklmnopqrstuvwxyz"
              "0123456789+/";
@@ -38,7 +38,8 @@ static inline bool is_base64(unsigned char c) {
   return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
-std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) 
+{
     std::string ret{};
   int i = 0;
   int j = 0;
@@ -61,17 +62,21 @@ std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_
 
   if (i)
   {
+    //널문자를 채워준다.
     for(j = i; j < 3; j++)
       char_array_3[j] = '\0';
 
+    //base64 변환
     char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
     char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
     char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
     char_array_4[3] = char_array_3[2] & 0x3f;
 
+    //변환된 base64를 넣어준다.
     for (j = 0; (j < i + 1); j++)
       ret += base64_chars[char_array_4[j]];
 
+    //문자열 끝부분은 '='를 넣어서 끝임을 확인하는 듯
     while((i++ < 3))
       ret += '=';
 
@@ -123,4 +128,69 @@ std::string base64_decode(const std::string_view encoded_string)
   }
 
   return ret;
+}
+
+void base64_decode(const std::string_view _encoded_string, unsigned char* _dest, unsigned int _destLen)
+{
+    //사이즈 유효성 검사
+    {
+        size_t dataSize = _encoded_string.find('=');
+        if (std::string_view::npos == dataSize)
+        {
+            dataSize = _encoded_string.size() / (size_t)4 * (size_t)3;
+        }
+
+        if (dataSize > (size_t)_destLen)
+        {
+            return;
+        }
+    }
+
+
+    memset(reinterpret_cast<void*>(_dest), 0, (size_t)_destLen);
+
+    int in_len = (int)_encoded_string.size();
+    int i = 0;
+    int j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+
+    unsigned int writeOffset = 0;
+
+    while (in_len-- && (_encoded_string[in_] != '=') && is_base64(_encoded_string[in_])) {
+        char_array_4[i++] = _encoded_string[in_]; in_++;
+        if (i == 4) {
+            for (i = 0; i < 4; i++)
+                char_array_4[i] = (unsigned char)base64_chars.find(char_array_4[i]);
+
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+            for (i = 0; (i < 3); i++)
+            {
+                _dest[writeOffset] = char_array_3[i];
+                ++writeOffset;
+            }
+            i = 0;
+        }
+    }
+
+    if (i) {
+        for (j = i; j < 4; j++)
+            char_array_4[j] = 0;
+
+        for (j = 0; j < 4; j++)
+            char_array_4[j] = (unsigned char)base64_chars.find(char_array_4[j]);
+
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+        for (j = 0; (j < i - 1); j++)
+        {
+            _dest[writeOffset] = char_array_3[j];
+            ++writeOffset;
+        }
+    }
 }
--- SampleCode/base64.h
+++ SampleCode/base64.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <string>
 
-std::string base64_encode(unsigned char const* , unsigned int len);
+std::string base64_encode(unsigned char const* src, unsigned int len);
 std::string base64_decode(const std::string_view s);
-
+void base64_decode(const std::string_view _encoded_string, unsigned char* _dest, unsigned int _destLen);
