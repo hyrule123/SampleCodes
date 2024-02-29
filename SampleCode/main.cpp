a868ddaf926e7fc88db16d8c4f33668956a5e78a
HWNOTE <ekdrn1@naver.com>
2024-02-29 16:02:40 +0900

    전에 작성했던거 틀린게 있어서 수정

--- SampleCode/main.cpp
+++ SampleCode/main.cpp
@@ -51,17 +51,17 @@ int main()
 	Matrix child = Matrix::Identity;
 	Matrix parent = Matrix::Identity;
 
-	Vector3 parentScale = Vector3(2.f);
+	Vector3 parentScale = Vector3(1.f, 2.f, 3.f);
 	Quaternion parentRot = Quaternion::CreateFromYawPitchRoll(Vector3(XM_PI / 1.f, XM_PI / 2.f, XM_PI / 3.f));
-	Vector3 parentPos = Vector3(3.f);
+	Vector3 parentPos = Vector3(3.f, 2.f, 1.f);
 		
 	parent *= Matrix::CreateScale(parentScale);
 	parent *= Matrix::CreateFromQuaternion(parentRot);
 	parent *= Matrix::CreateTranslation(parentPos);
 
-	Vector3 childScale = Vector3(3.f);
+	Vector3 childScale = Vector3(4.f, 3.f, 2.f);
 	Quaternion childRot = Quaternion::CreateFromYawPitchRoll(Vector3(XM_PI * 3.1235f, XM_PI * 12.3853f, XM_PI * 8.192f));
-	Vector3 childPos = Vector3(4.f);
+	Vector3 childPos = Vector3(4.f, 8.f, 12.f);
 	child *= Matrix::CreateScale(childScale);
 	child *= Matrix::CreateFromQuaternion(childRot);
 	child *= Matrix::CreateTranslation(childPos);
@@ -72,8 +72,9 @@ int main()
 	Quaternion childWorldRot = childRot * parentRot;
 	Vector3 chileWorldPos = childWorld.Translation();
 
-	//행렬끼리 곱한 결과와 world SRT를 각각 구한 값이 동일한지 확인
-	//결론: 동일(World Translation 값만 알고 있다면 동일한 행렬을 만들어낼 수 있는것을 확인함)
+	//행렬끼리 곱한 결과(SS * RR * Tw)와 world SRT를 각각 구한 값이 동일한지 확인
+	//결론: 크기 행렬이 X, Y, Z 모두 동일 배율일 경우 동일하지만, 
+	// X, Y, Z 모두 다른 배율일 경우 동일하지 않음.
 	Matrix createFromWorldValues = Matrix::CreateScale(childWorldScale)
 		* Matrix::CreateFromQuaternion(childWorldRot)
 		* Matrix::CreateTranslation(chileWorldPos);
@@ -84,7 +85,53 @@ int main()
 	PrintMatrix("createFromWorldValues", createFromWorldValues);
 
 	MatrixCmp(childWorld, createFromWorldValues);
+	//========================================================================
 
+
+	//Scale 없이 만든 Matrix와 Scale 포함해서 만든 Matrix에  1/ scale matrix를 곱한 결과가 같은지?
+	//결론: 안됨. Scale이 XYZ 모두 동일한 배율이면 되는데 그렇지 않을경우 안됨.
+	Matrix childWorldScaleInv = Matrix::CreateScale(Vector3(1.f) / childWorldScale);
+	Matrix childWorldScaleInversed = childWorld * childWorldScaleInv;
+	//childWorldWithoutTranslation.Translation(Vector3(0.f, 0.f, 0.f));
+
+
+	Matrix createWithoutScale = Matrix::CreateScale(1.f)
+		* Matrix::CreateFromQuaternion(childWorldRot)
+		* Matrix::CreateTranslation(childWorld.Translation());
+
+	
+	
+	PrintMatrix("childWorldScaleInversed", childWorldScaleInversed);
+	PrintMatrix("createWithoutScale", createWithoutScale);
+
+	MatrixCmp(childWorldScaleInversed, createWithoutScale);
+	//======================================================================================
+
+	//Scale 없이 만든 Matrix와 World Matrix를 Normalize한 결과는 같은가?
+	createWithoutScale;
+	Matrix normalizedWorldMat = childWorld;
+	normalizedWorldMat.Up(normalizedWorldMat.Up().Normalize());
+	normalizedWorldMat.Right(normalizedWorldMat.Right().Normalize());
+	normalizedWorldMat.Forward(normalizedWorldMat.Forward().Normalize());
+
+	PrintMatrix("createWithoutScale", createWithoutScale);
+	PrintMatrix("normalizedWorldMat", normalizedWorldMat);
+
+	MatrixCmp(createWithoutScale, normalizedWorldMat);
+	//======================================================================================
+
+
+	Matrix childWorld_ScaleNormalized = childWorld;
+	childWorld_ScaleNormalized.Right(childWorld_ScaleNormalized.Right().Normalize());
+	childWorld_ScaleNormalized.Up(childWorld_ScaleNormalized.Up().Normalize());
+	childWorld_ScaleNormalized.Forward(childWorld_ScaleNormalized.Forward().Normalize());
+
+	PrintMatrix("childWorldScaleInversed", childWorldScaleInversed);
+	PrintMatrix("childWorld_ScaleNormalized", childWorld_ScaleNormalized);
+
+	MatrixCmp(childWorldScaleInversed, childWorld_ScaleNormalized);
+
+	//======================================================================================
 
 	//내장된 Decompose 함수를 통한 SRT 분해
 	Vector3 decomposedWorldScale{};
@@ -98,7 +145,7 @@ int main()
 	
 	Matrix invWorldRot = Matrix::CreateFromQuaternion(childWorldRot);
 	Matrix childWorld_multiplyInvRot = invWorldRot * childWorld;
-
+	
 	Matrix childWorld_recomposed_withoutRot = 
 		Matrix::CreateScale(decomposedWorldScale)
 		* Matrix::CreateFromQuaternion(Quaternion::Identity)
