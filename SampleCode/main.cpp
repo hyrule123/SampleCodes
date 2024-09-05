f7c77592146beb7ca3f1e8ae9edea368871669a8
HWNOTE <ekdrn1@naver.com>
2024-09-05 15:28:04 +0900

    가변인자 템플릿 함수 사용법

--- SampleCode/main.cpp
+++ SampleCode/main.cpp
@@ -3,28 +3,34 @@
 #include <functional>
 #include <memory>
 #include <iostream>
+#include <string>
 
-void foo(std::unique_ptr<int>&& i) noexcept
-{
-	if (i)
-	{
-		std::cout << *i << std::endl;
-	}
+std::vector<std::string> vecstr = { "1", "2", "3", "4", "5" };
+std::vector<std::function<void()>> vec;
+
+void forw() {
+}
+
+template <typename T, typename... ARGS>
+void forw(T&& t, ARGS&&... args) {
+	vec.push_back(
+		[t]()->void {
+			std::cout << t << std::endl;
+		}
+	);
+
+	forw(std::forward<ARGS>(args)...);
 }
 
 int main()
 {
-	std::vector<int> vec1{ 1, 2, 3, 4, 5 };
-	std::vector<int> vec2{ 5, 6 ,7 ,8 ,9 };
+	forw(vecstr[0], vecstr[1], vecstr[2], vecstr[3], vecstr[4]);
 
-	//from 벡터의 사이즈를 알 수 있는 경우 O(n)이라고 한다.
-	auto iter = vec1.insert(vec1.end(), std::make_move_iterator(vec2.begin() + 1), std::make_move_iterator(vec2.end()));
+	vecstr.clear();
 
-	//iter 반환값: 옮겨붙인 곳의 시작점 iterator
-	std::cout << *iter << std::endl;
-
-	//옮기고 난 뒤에 vec2는 undefined state가 되므로 clear 또는 옮겨진 부분 뒷쪽을 제거해주어야 한다.
-	vec2.resize((vec2.begin() + 1) - vec2.begin());
+	for (const auto& f : vec) {
+		f();
+	}
 
 	return 0;
 }
