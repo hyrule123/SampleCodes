ccefe669affa7a77d92e68b9cda7478a0eb243ef
HW <ekdrn1@naver.com>
2026-07-10 09:04:20 +0900

    C++20 이상용 Prehashed Unordered Map(std::string 추가)

--- SampleCode/main.cpp
+++ SampleCode/main.cpp
@@ -4,16 +4,19 @@
 #include <unordered_map>
 
 // 1. 컴파일 타임/런타임 하이브리드 키 (수정됨)
-struct PrehashedKey {
+struct PreHashedString {
     std::string_view str;
     size_t hash_value;
 
     // A. 컴파일 타임 리터럴 전용 (배열 참조가 Exact Match라 리터럴 입력 시 무조건 이것이 선택됨)
     template <size_t N>
-    consteval PrehashedKey(const char(&s)[N])
+    consteval PreHashedString(const char(&s)[N])
         : str(s, N - 1), hash_value(hash_str(std::string_view(s, N - 1))) {}
 
-	PrehashedKey(const std::string_view s)
+	PreHashedString(const std::string_view s)
+		: str(s), hash_value(hash_str(s)) {}
+
+    PreHashedString(const std::string& s)
 		: str(s), hash_value(hash_str(s)) {}
 
     static constexpr size_t hash_str(std::string_view s) {
@@ -26,6 +29,13 @@ struct PrehashedKey {
     }
 };
 
+struct PreHashedStringRuntime {
+	std::string str;
+	size_t hash_value;
+	PreHashedStringRuntime(const std::string& s)
+		: str(s), hash_value(PreHashedString::hash_str(s)) {}
+};
+
 // ---------------------------------------------------------
 // 2. 투명한(Transparent) 해시 펑터 (모호성 완벽 제거 버전)
 // ---------------------------------------------------------
@@ -34,23 +44,28 @@ struct TransparentHash {
     using is_transparent = void;
 
     // std::string 정확한 매칭 (변환 없이 바로 해싱)
-    size_t operator()(const std::string& s) const { return PrehashedKey::hash_str(s); }
+    size_t operator()(const std::string& s) const { return PreHashedString::hash_str(s); }
 
     // 기존 매칭들
-    size_t operator()(std::string_view s) const { return PrehashedKey::hash_str(s); }
-    size_t operator()(const PrehashedKey& pk) const { return pk.hash_value; }
+    size_t operator()(std::string_view s) const { return PreHashedString::hash_str(s); }
+    size_t operator()(const PreHashedString& pk) const { return pk.hash_value; }
 };
 
 struct TransparentEqual {
     using is_transparent = void;
 
+	// 1. std::string 그룹
+    bool operator()(const std::string& lhs, const std::string& rhs) const { return lhs == rhs; }
+    bool operator()(const std::string& lhs, const PreHashedString& rhs) const { return lhs == rhs.str; }
+    bool operator()(const PreHashedString& lhs, const std::string& rhs) const { return lhs.str == rhs; }
+
     // 2. std::string_view 그룹
     bool operator()(std::string_view lhs, std::string_view rhs) const { return lhs == rhs; }
-    bool operator()(std::string_view lhs, const PrehashedKey& rhs) const { return lhs == rhs.str; }
-    bool operator()(const PrehashedKey& lhs, std::string_view rhs) const { return lhs.str == rhs; }
+    bool operator()(std::string_view lhs, const PreHashedString& rhs) const { return lhs == rhs.str; }
+    bool operator()(const PreHashedString& lhs, std::string_view rhs) const { return lhs.str == rhs; }
 
-    // 3. PrehashedKey 전용 그룹
-    bool operator()(const PrehashedKey& lhs, const PrehashedKey& rhs) const {
+    // 3. PreHashedString 전용 그룹
+    bool operator()(const PreHashedString& lhs, const PreHashedString& rhs) const {
         return lhs.hash_value == rhs.hash_value && lhs.str == rhs.str;
     }
 };
@@ -65,7 +80,7 @@ private:
 public:
     // [단일 operator[]] 
     // 리터럴을 넣으면 컴파일 타임 해싱, std::string을 넣으면 런타임 해싱이 자동으로 분기됩니다.
-    ValueType& operator[](const PrehashedKey& key) {
+    ValueType& operator[](const PreHashedString& key) {
         auto it = map_.find(key);
         if (it != map_.end()) {
             return it->second; // 이미 있으면 즉시 반환 (비용 0)
@@ -76,7 +91,7 @@ public:
     }
 
     // [단일 find]
-    ValueType* find(const PrehashedKey& key) {
+    ValueType* find(const PreHashedString& key) {
         auto it = map_.find(key);
         return it != map_.end() ? &it->second : nullptr;
     }
@@ -89,6 +104,8 @@ public:
 int main() {
     HybridStringMap<int> GameData;
 
+	PreHashedString key = "PlayerHp";
+
     // ⭕️ 완벽하게 consteval로 작동합니다! (함수 외부에서 변환되므로 에러 없음)
     GameData["PlayerHp"] = 100;
 
@@ -96,7 +113,7 @@ int main() {
 
     std::string dynamicKey = "MonsterHp";
     // ⭕️ 런타임 해싱으로 자연스럽게 작동합니다!
-    GameData[std::string_view(dynamicKey)] = 200;
+    GameData[dynamicKey] = 200;
 
     if (int* hp = GameData.find("PlayerHp")) { // consteval 탐색
         std::cout << "Player HP: " << *hp << "\n";
